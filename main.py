46d93434821fd7ddd5a909238050a12a4f3eaafe
chuckgao <chuckgao@mini.local>
2024-11-27 18:15:42 +0800

    feat: add run mode

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests, time
 import pandas as pd
 import bech32
+import argparse
 
+
+SUPPORTED_TOKENS = ['TREAT', 'TRICK', 'NOSTR', 'BURGER', 'TNA', 'NODE1', 'USDT']
 
 def get_token_id(token: str) -> str:
   token = token.upper()
@@ -30,7 +33,7 @@ def get_order_history_v1(token: str, page: int, count: int) -> dict:
     return
 
   try:
-    ['TREAT', 'TRICK', 'NOSTR', 'TNA', 'BURGER'].index(token)
+    SUPPORTED_TOKENS.index(token)
   except ValueError:
     print('invalid token')
     return
@@ -120,74 +123,83 @@ def encode_npub(hex_key):
     return hex_key
 
 if __name__ == '__main__':
-  token = 'usdt'
+  parser = argparse.ArgumentParser(description='Lnfi Crawler')
+  parser.add_argument('action', choices=['get-orders', 'get-holders'], help='action to perform')
+  parser.add_argument('token', choices=SUPPORTED_TOKENS, help='token to get')
+
+  args = parser.parse_args()
+  token = args.token
   page = 1
   size = 100
 
-  # 获取历史订单
-  # count = 0
-  # orders = []
-  # orders_file_path = f'./{token}_orders_history.csv'
+  if args.action == 'get-orders':
+    # 获取历史订单
+    count = 0
+    orders = []
+    orders_file_path = f'./{token}_orders_history.csv'
 
-  # while True:
-  #   print(f'fetching {(page - 1) * size} ~ {page * size} orders')
-  #   order_history = get_order_history_v1(token, page, size)
-  #   if order_history.get('code', -1) == 0:
-  #     print(f'fetching {(page - 1) * size} ~ {page * size} orders success')
-  #     _orders = order_history.get('data', {}).get('orderPOS', [])
-  #     _count = order_history.get('data', {}).get('sumCount', 0)
-  #     print(f'total {_count} orders; current get {len(orders)} orders\n')
+    while True:
+      print(f'fetching {(page - 1) * size} ~ {page * size} orders')
+      order_history = get_order_history_v1(token, page, size)
+      if order_history.get('code', -1) == 0:
+        print(f'fetching {(page - 1) * size} ~ {page * size} orders success')
+        _orders = order_history.get('data', {}).get('orderPOS', [])
+        _count = order_history.get('data', {}).get('sumCount', 0)
+        print(f'total {_count} orders; current get {len(orders)} orders\n')
 
-  #     orders.extend(_orders)
+        orders.extend(_orders)
 
-  #     if len(orders) >= _count:
-  #       df = pd.DataFrame(orders)
-  #       df.to_csv(orders_file_path, index=False)
-  #       break
+        if len(orders) >= _count:
+          df = pd.DataFrame(orders)
+          df.to_csv(orders_file_path, index=False)
+          break
 
-  #     page += 1
-  #     time.sleep(0.5)
-  #   else:
-  #     print(f'fetching failed, reason: {order_history.get('data', '')}')
+        page += 1
+        time.sleep(0.5)
+      else:
+        print(f'fetching failed, reason: {order_history.get('data', '')}')
+  elif args.action == 'get-holders':
+    # 获取持有者
+    count = 0
+    holders = []
+    holders_file_path = f'./{token}_holders.csv'
 
-  # 获取持有者
-  count = 0
-  holders = []
-  holders_file_path = f'./{token}_holders.csv'
-
-  # 获取持有者总数
-  print('fetching holders summary')
-  holder_summary = get_holder_summary(token)
-  if holder_summary.get('code', -1) == 0:
-    count = holder_summary.get('data', {}).get('data', {}).get('holderCount', 0)
-    if count == 0:
-      print('no holders')
-      exit(0)
+    # 获取持有者总数
+    print('fetching holders summary')
+    holder_summary = get_holder_summary(token)
+    if holder_summary.get('code', -1) == 0:
+      count = holder_summary.get('data', {}).get('data', {}).get('holderCount', 0)
+      if count == 0:
+        print('no holders')
+        exit(0)
+      else:
+        print(f'total {count} holders\n')
     else:
-      print(f'total {count} holders\n')
-  else:
-    print(f'fetching holders summary failed, reason: {holder_summary.get('data', '')}')
-    exit(1)
-
-  while True:
-    print(f'fetching {(page - 1) * size} ~ {page * size} holders')
-    holder_res = get_holder(token, page, size)
-    if holder_res.get('code', -1) == 0:
-      print(f'fetching {(page - 1) * size} ~ {page * size} holders success')
-      holders.extend(holder_res.get('data', {}).get('data', []))
-      print(f'total {count} holders; current get {len(holders)} holders\n')
-
-      if len(holders) >= count:
-        df = pd.DataFrame(holders)
-        df['owner'] = df['owner'].apply(encode_npub)
-        df.to_csv(holders_file_path, index=False)
-        break
-
-      page += 1
-      time.sleep(0.5)
-    else:
-      print(f'fetching holders failed, reason: {holder_res.get('data', '')}')
+      print(f'fetching holders summary failed, reason: {holder_summary.get('data', '')}')
       exit(1)
+
+    while True:
+      print(f'fetching {(page - 1) * size} ~ {page * size} holders')
+      holder_res = get_holder(token, page, size)
+      if holder_res.get('code', -1) == 0:
+        print(f'fetching {(page - 1) * size} ~ {page * size} holders success')
+        holders.extend(holder_res.get('data', {}).get('data', []))
+        print(f'total {count} holders; current get {len(holders)} holders\n')
+
+        if len(holders) >= count:
+          df = pd.DataFrame(holders)
+          df['owner'] = df['owner'].apply(encode_npub)
+          df.to_csv(holders_file_path, index=False)
+          break
+
+        page += 1
+        time.sleep(0.5)
+      else:
+        print(f'fetching holders failed, reason: {holder_res.get('data', '')}')
+        exit(1)
+  else:
+    print('invalid action')
+    exit(1)
 
 
 
